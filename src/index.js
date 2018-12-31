f8dd8c8d0540c37dc7a69606fd4a65313af111a4
Halasi Tamas <trusted.tomato@gmail.com>
2018-12-31 17:58:21 +0100

    add input checking & fix bugs

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import Sortable from 'sortablejs'
 
 /*--- YouTube API wrappers  ---*/
 // ouch why is this YouTube iframe api so bad...
-const createYtPlayer = (videoId, container) => new Promise(resolve => {
+const createYtPlayer = (videoId, container) => new Promise((resolve, reject) => {
 	const el = document.createElement('iframe');
 	el.frameBorder = 0;
-	el.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+	el.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope');
 	el.setAttribute('donotallowfullscreen', '');
 	el.src = 'https://www.youtube.com/embed/' + videoId + '?disablekb=1&enablejsapi=1&controls=0&origin=' + encodeURIComponent(window.origin);
+	console.log('creating a yt player...');
+	el.style.display = 'none';
 	container.appendChild(el);
+
+	const _reject = err => {
+		console.error('error on yt player creation, removing element...');
+		container.removeChild(el);
+		reject(err);
+	};
 	
 	const player = new YT.Player(el, {
 		playerVars: {
@@ -21,7 +29,16 @@ const createYtPlayer = (videoId, container) => new Promise(resolve => {
 			'onReady': () => {
 				setBestSoundQuality(player);
 				player.setVolume(100);
-				resolve(player);
+				if(player.getVideoData().title){
+					el.style.display = '';
+					resolve(player);
+				}else{
+					// if the title is falsy, there must be something wrong (onError doesn't seem to work)
+					_reject(1);
+				}
+			},
+			'onError': ({data}) => {
+				_reject(data);
 			}
 		}
 	});
@@ -31,6 +48,7 @@ const destroyYtPlayer = player => {
 	// currently, the doc says that the destroy method just removes the iframe
 	// but IRL it replaces it with a div, which is not desired,
 	// so I just remove the iframe instead.
+	console.log('removing iframe...');
 	player.getIframe().remove();
 };
 
@@ -48,10 +66,8 @@ const getYtVideoTitle = (function(){
 			const title = player.getVideoData().title;
 			destroyYtPlayer(player);
 			return title;
-		})
+		});
 })();
-
-const expFunc = x => Math.cos(x * 0.5 * Math.PI);
 
 const youtubeApiReady = new Promise(resolve => {
 	window.onYouTubeIframeAPIReady = resolve;
@@ -62,14 +78,27 @@ const getYtVideoId = url =>
 
 const fade = (player, targetVolume) => {
 	let volume = player.getVolume();
-
 };
 
 
 /*--- helper functions ---*/
-const parseYtVideoId = input => getYtVideoId(new URL(input)) || input;
+const parseYtVideoId = input =>
+	nullOnCatch(() =>
+		// if its not a valid url, it will error
+		getYtVideoId(new URL(input))
+	) || input;
 
 const last = arr => arr[arr.length - 1];
+
+const expFunc = x => Math.cos(x * 0.5 * Math.PI);
+
+const nullOnCatch = func => {
+	try{
+		return func();
+	}catch(err){
+		return null;
+	}
+};
 
 customElements.define('li-track', class extends HTMLLIElement{
 	constructor(){
@@ -94,6 +123,10 @@ customElements.define('li-track', class extends HTMLLIElement{
 		this.getTitle()
 			.then(title => {
 				text.textContent = title;
+			})
+			.catch(err => {
+				console.error('cannot load player because of:', err);
+				this.remove();
 			});
 		this.appendChild(children);
 	}
@@ -113,9 +146,26 @@ customElements.define('ul-playlist', class extends HTMLUListElement{
 			scrollSpeed: 2,
 			filter: '.remove',
 			onFilter: e => {
+				if(this.currentChild === e.item){
+					this.next();
+				}
 				e.item.remove();
 			}
 		});
+		// detect
+		new MutationObserver(mutations => {
+			// the currentChild might still be there just elsewhere
+			if(this.contains(this.currentChild)) return;
+			for(const mutation of mutations){
+				if([...mutation.removedNodes].some(removedNode =>
+					removedNode === this.currentChild
+				)){
+					console.log('removed current node, starting next one');
+					// previous/next sibling might be a text node, so I had to be tricky here
+					this.start(this.nextElement(mutation.previousSibling));
+				}
+			}
+		}).observe(this, {childList: true});
 	}
 	fade(){
 		if(this.fading) return;
@@ -129,10 +179,10 @@ customElements.define('ul-playlist', class extends HTMLUListElement{
 		}
 	}
 	nextElement(child){
-		return child && (child.nextElementSibling || this.firstElementChild);
+		return (child && child.nextElementSibling) || this.firstElementChild;
 	}
 	prevElement(child){
-		return child && (child.previousElementSibling || this.lastElementChild);
+		return (child && child.previousElementSibling) || this.lastElementChild;
 	}
 	start(child){
 		clearTimeout(this.endTimeout);
@@ -141,12 +191,13 @@ customElements.define('ul-playlist', class extends HTMLUListElement{
 		}
 		this.currentChild = child;
 		console.log(child);
-		if(child === null) return false;
+		if(child === null){
+			this.players.forEach(destroyYtPlayer);
+			return false;
+		}
 		child.dataset.playing = true;
 		child.createYtPlayer(this.videoContainer).then(async player => {
-			for(const player of this.players){
-				destroyYtPlayer(player);
-			}
+			this.players.forEach(destroyYtPlayer);
 			this.players = [player];
 			player.addEventListener('onStateChange', e => {
 				switch(e.data){
@@ -213,6 +264,8 @@ customElements.define('ul-playlist', class extends HTMLUListElement{
 	}
 },{extends: 'ul'});
 
+const ytIdRegex = /^[-_0-9A-Za-z]{11}$/;
+
 
 /*--- the big bang ---*/
 (async function(){
@@ -225,7 +278,11 @@ customElements.define('ul-playlist', class extends HTMLUListElement{
 	addForm.addEventListener('submit', e => {
 		e.preventDefault();
 		const id = parseYtVideoId(addInput.value);
-		playlistUl.add(id);
+		if(typeof id === 'string' && ytIdRegex.test(id)){
+			playlistUl.add(id);
+		}else{
+			console.error('invalid id:', id);
+		}
 	});
 
 	const prev = document.getElementById('prev');
